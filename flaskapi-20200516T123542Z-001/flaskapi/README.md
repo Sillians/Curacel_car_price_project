81007cbb5c42ac3bda35f249c9c5e1744429a423
Olaniyiajayi2 <olaniyiezekieltobi@gmail.com>
2020-05-21 14:52:05 +0100

    first commit

---
+++ flaskapi-20200516T123542Z-001/flaskapi/README.md
@@ -0,0 +1,3 @@
+Read and View my API Documentation.
+
+https://speeding-flare-199791.postman.co/collections/11423943-9142135d-4c5d-43df-895e-1d55f94503fb?version=latest&workspace=57c7d5cb-52fd-44ba-8d57-b3733af8f526
---
+++ flaskapi-20200516T123542Z-001/flaskapi/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+import numpy as np
+import joblib
+import pickle
+# from sklearn.externals import joblib
+
+
+app = Flask(__name__)
+
+model = pickle.load(open("models/randomforestregressor/model.pkl", 'rb'))
+print('Loaded')
+
+features = pickle.load(open("features/featuresdata/features.pkl", 'rb'))
+print('Features loaded')
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+
+    if request.method == 'POST':
+
+        json_data = request.json
+        df = pd.DataFrame(json_data)
+        df = pd.get_dummies(df)
+        
+        df_ = df.reindex(columns = features, fill_value = 0)
+
+        pred = list(model.predict(df_))
+
+        return jsonify({'prediction': str(pred)})
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 2000)
